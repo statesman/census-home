23a73467306b73f64c3191a267f2f8f07c26b887
critmcdonald <christian@cmcdonald.com>
2016-06-22 16:28:27 -0500

    adding imagesloaded and fixed masonry stack css

--- src/js/main.js
+++ src/js/main.js
@@ -11,8 +11,13 @@
     $('.grid').masonry({
       // options
       itemSelector: '.grid-item',
-      columnWidth: 200
+      // columnWidth: 200
     });
+
+    $grid.imagesLoaded().progress( function() {
+      $grid.masonry('layout');
+    });
+
 
 //    $(document).ready(function() {
 //        $.getJSON(data_url, function(d) {
